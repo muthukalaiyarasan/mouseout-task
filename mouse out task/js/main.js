fb61e78b0b8654bd1b0d58f304c10a618b3192ea
muthukalaiyarasan <132567401+muthukalaiyarasan@users.noreply.github.com>
2023-09-04 12:30:51 +0530

    Add files via upload

---
+++ mouse out task/js/main.js
@@ -0,0 +1,9 @@
+let int=document.getElementById("input")
+let para=document.getElementById("head")
+      
+	    input.addEventListener("mouseout",change)
+		    
+			 function change(){
+				 para.style.color=input.value
+				  console.log(input.value)
+			 }
